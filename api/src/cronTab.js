20ef5ee9ea09cb2391f4ed780d80e3fe5e518c2f
Idric Evarne <idric.evarne@gmail.com>
2024-08-12 15:39:00 +0400

    Finalisation du code gestion CO2 du back office.

--- api/src/cronTab.js
+++ api/src/cronTab.js
@@ -1,26 +1,20 @@
 const schedule = require("node-schedule");
 const childProcess = require("child_process");
+const invoked = false;
+const process = childProcess.fork(scriptPath);
 
 //*! 1) Gestion Air.
 
 //? Calcules.
 
-//const gestionAir = schedule.scheduleJob(" * * * * * ", () => {
-const gestionAir = schedule.scheduleJob(" */10 * * * * ", () => {
+const gestionAir = schedule.scheduleJob("*/5 * * * *", () => {
   function runScript(scriptPath, callback) {
-    //* keep track of whether callback has been invoked to prevent multiple invocations
-    let invoked = false;
-
-    let process = childProcess.fork(scriptPath);
-
-    //* listen for errors as they may prevent the exit event from firing
     process.on("error", function (err) {
       if (invoked) return;
       invoked = true;
       callback(err);
     });
 
-    //* execute the callback once the process has finished running
     process.on("exit", function (code) {
       if (invoked) return;
       invoked = true;
@@ -29,14 +23,83 @@ const gestionAir = schedule.scheduleJob(" */10 * * * * ", () => {
     });
   }
 
-  //* Now we can run a script and invoke a callback when complete, e.g.
   runScript(
     "/home/pi/Desktop/champiBack_V4/api/src/services/gestionAir/gestionAir.js",
     function (err) {
       if (err) throw err;
-      console.log(`🔴 SUCCÈS | Gestions Air | fin du script`);
+      // console.log(
+      //   jaune,
+      //   '[ GESTION AIR CRON TAB  ] CALCULES Calcules sont  terminés'
+      // );
     }
   );
 });
 
-//? ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//? -------------------------------------------------
+
+//*! 2) Gestion humidité.
+
+//? Calcules.
+
+const gestionHum = schedule.scheduleJob("*/15 * * * *", () => {
+  // const gestionHum = schedule.scheduleJob(' _/10 \* \* \* \* ', () => {
+
+  function runScript(scriptPath, callback) {
+    process.on("error", function (err) {
+      if (invoked) return;
+      invoked = true;
+      callback(err);
+    });
+
+    process.on("exit", function (code) {
+      if (invoked) return;
+      invoked = true;
+      var err = code === 0 ? null : new Error("exit code " + code);
+      callback(err);
+    });
+  }
+
+  runScript(
+    "/home/pi/Desktop/champiBack_V4/api/src/services/gestionHumidite/gestionHumidite.js",
+    function (err) {
+      if (err) throw err;
+      // console.log(
+      //   bleu,
+      //   '[ GESTION HUM CRON TAB  ] GESTION HUMIDITÉ finished running some-script.js'
+      // );
+    }
+  );
+});
+
+//? -------------------------------------------------
+
+//*! 3) Gestion Co2
+
+//? Calculs.
+
+const gestionCo2 = schedule.scheduleJob(" 01 * * * * ", () => {
+  function runScript(scriptPath, callback) {
+    process.on("error", function (err) {
+      if (invoked) return;
+      invoked = true;
+      callback(err);
+    });
+
+    process.on("exit", function (code) {
+      if (invoked) return;
+      invoked = true;
+      var err = code === 0 ? null : new Error("exit code " + code);
+      callback(err);
+    });
+  }
+
+  runScript("./gestion/gestionCo2/gestionCo2.js", function (err) {
+    if (err) throw err;
+    // console.log(
+    //   cyan,
+    //   '[ GESTION CO2 CRON TAB  ] GESTION CO2 finished running some-script.js'
+    // );
+  });
+});
+
+//? -------------------------------------------------
---
+++ api/src/services/gestionCo2/gestionCo2.js
@@ -0,0 +1,297 @@
+require("dotenv").config();
+const http = require("http");
+const { resolve } = require("path");
+const sequelize = require("sequelize");
+const db = require("../../models");
+const gestionCo2Models = db.gestionCo2;
+const gestionCo2DataModels = db.gestionCo2Data;
+const numSalle = require("../../utils/numSalle/configNumSalle");
+
+//? Switch entre le mode développement et le mode production.
+
+let masterURL;
+
+const switchEntreDeveloppementEtProduction = () => {
+  return new Promise((resolve, reject) => {
+    try {
+      if (process.env.CHAMPIBACK_STATUS === "developpement") {
+        masterURL = "http://192.168.0.10:5000/getCO2/" + numSalle;
+        resolve(console.log("MODE DÉVELOPPEMENT ACTIF"));
+      }
+
+      if (process.env.CHAMPIBACK_STATUS === "production") {
+        masterURL = "http://localhost:5000/getCo2/" + numSalle;
+        resolve(console.log("MODE PRODUCTION ACTIF"));
+      }
+    } catch (error) {
+      reject(console.log("🟠 TRY CATCH ERROR : my reject :", error));
+    }
+  });
+};
+
+//? -------------------------------------------------
+
+//? Demande de mesure à la master.
+
+let data;
+let tauxCo2;
+
+const demandeDeMesureMaster = () => {
+  return new Promise((resolve, reject) => {
+    try {
+      http
+        .get(masterURL, (resp) => {
+          resp.on("data", (chunk) => {
+            data += chunk;
+            console.log(
+              "✅ SUCCÈS | Gestion CO2 1 Demande de mesure à la master | RESP.ON DATA : ",
+              data
+            );
+          });
+
+          resp.on("end", () => {
+            tauxCo2 = parseFloat(data.split('undefined"')[1]);
+            console.log(
+              "✅ SUCCÈS | Gestion CO2 1 Demande de mesure à la master | RESP.ON END : ",
+              tauxCo2
+            );
+          });
+        })
+
+        .on("response", function (resp) {
+          if (resp.statusCode === 200) {
+            resolve(
+              console.log(
+                "✅ SUCCÈS | Gestion CO2 2 Demande de mesure à la master | RESP.ON RESPONSE | Status Code ",
+                resp.statusCode
+              )
+            );
+          } else {
+            reject(
+              console.log(
+                "🔴 ERROR | Gestion CO2 2 Demande de mesure à la master | Status Code | RESP.ON RESPONSE ",
+                resp.statusCode
+              )
+            );
+          }
+        })
+
+        .on("error", (err) => {
+          reject(
+            console.log(
+              "🔴 ERROR | Gestion CO2 2 Demande de mesure à la master | RESP.ON ERREOR : ",
+              err.message
+            )
+          );
+        });
+    } catch (error) {
+      reject(
+        console.log(
+          "🟠 TRY CATCH ERROR : Demande de mesure à la master : ",
+          error
+        )
+      );
+    }
+  });
+};
+
+//? -----------------------------------------------------------
+
+//? Récupération de la consigne Co2.
+
+let consigneCo2;
+let pasCo2;
+let objectifCo2;
+
+const recuperationDeLaConsigneCo2 = () => {
+  return new Promise((resolve, reject) => {
+    try {
+      gestionCo2DataModels
+        .findOne({
+          attributes: [[sequelize.fn("max", sequelize.col("id")), "maxid"]],
+          raw: true,
+        })
+        .then((id) => {
+          // console.log(id.maxid);
+          gestionCo2DataModels
+            .findOne({
+              where: { id: id.maxid },
+            })
+            .then((result) => {
+              console.log("DATA Brute" + result);
+              lastId = result.id;
+
+              consigneCo2 = result.consigneCo2;
+              console.log("La consigne est : ", consigneCo2);
+
+              pasCo2 = result.pasCo2;
+              console.log("Pas :      ", pasCo2);
+
+              objectifCo2 = result.objectifCo2;
+              console.log("Objectif : ", objectifCo2);
+
+              resolve({ consigneCo2, pasCo2, objectifCo2 });
+            });
+        });
+    } catch (error) {
+      reject(
+        console.log(
+          "🟠 TRY CATCH ERROR : Récupération de la consigne Co2 :",
+          error
+        )
+      );
+    }
+  });
+};
+
+//? -----------------------------------------------------------
+
+//? Calcule du delta Co2.
+
+let deltaCo2;
+
+const calculeDuDeltaCo2 = () => {
+  return new Promise((resolve, reject) => {
+    try {
+      if (typeof tauxCo2 !== "number" || typeof consigne !== "number") {
+        console.error("Erreur : tauxCo2 ou consigne ne sont pas des nombres.");
+      } else {
+        deltaCo2 = tauxCo2 - consigne;
+        resolve(console.log("deltaCo2 :", deltaCo2));
+      }
+    } catch (error) {
+      reject(console.log("🟠 TRY CATCH ERROR : Calcule du delta Co2 :", error));
+    }
+  });
+};
+
+//? -----------------------------------------------------------
+
+//? Construction de la valeur de l'axe x.
+
+let dateDemarrageCycle;
+let jourDuCycle;
+let heureDuCycle;
+let minuteDuCycle;
+let heureMinute;
+let valeurAxeX;
+
+let constructionDeLaValeurDeLaxeX = () => {
+  return new Promise((resolve, reject) => {
+    try {
+      gestionCo2Models
+        .findOne({
+          attributes: [[sequelize.fn("max", sequelize.col("id")), "maxid"]],
+          raw: true,
+        })
+        .then((id) => {
+          if (!id) {
+            throw new Error("No max ID found");
+          }
+
+          return gestionCo2Models.findOne({
+            where: { id: id.maxid },
+          });
+        })
+        .then((result) => {
+          if (!result) {
+            throw new Error("No record found with max ID");
+          }
+
+          dateDemarrageCycle = new Date(result.dateDemarrageCycle);
+
+          console.log(
+            "✅ SUCCÈS | Gestion Co2 2 | Date de démarrage du cycle :",
+            dateDemarrageCycle
+          );
+
+          const nbJourBrut =
+            new Date().getTime() - dateDemarrageCycle.getTime();
+          jourDuCycle = Math.round(nbJourBrut / (1000 * 3600 * 24)) + 1;
+          heureDuCycle = new Date().getHours();
+          minuteDuCycle = new Date().getMinutes();
+
+          if (minuteDuCycle < 10) {
+            minuteDuCycle = `0${minuteDuCycle}`;
+          }
+
+          heureMinute = `${heureDuCycle}h${minuteDuCycle}`;
+          valeurAxeX = `Jour ${jourDuCycle} - ${heureMinute}`;
+
+          console.log(
+            "✅ SUCCÈS | Gestion Co2 3 | Construction de la valeur de l'axe X : ",
+            valeurAxeX
+          );
+
+          resolve(valeurAxeX);
+        })
+        .catch((error) => {
+          console.log(
+            "🔴 ERROR | Gestion Co2 | Construction de la valeur de l'axe X",
+            error
+          );
+          reject(error);
+        });
+    } catch (error) {
+      console.log(
+        "🔴 ERROR | Gestion Co2 | Construction de la valeur de l'axe X",
+        error
+      );
+      reject(error);
+    }
+  });
+};
+
+//? --------------------------------------------------
+
+//? Enregistrement en base de donnes.
+
+const enregistrementEnBaseDeDonnes = () => {
+  return new Promise((resolve, reject) => {
+    gestionCo2Models
+      .create({
+        tauxCo2: tauxCo2,
+        deltaCo2: deltaCo2,
+        daysCo2: daysCo2,
+        heuresCo2: heuresCo2,
+        consigne: consigneCo2,
+        valeurAxeX: valeurAxeX,
+        jourDuCycle: jourDuCycle,
+      })
+      .then(() => {
+        console.log("Données transférées à la base de données gestion_co2s.");
+      })
+      .catch((error) => {
+        console.log(
+          cyan,
+          "[ GESTION CO2 CALCULES  ] Erreur dans le processus d’enregistrement dans la base gestion_co2s",
+          error
+        );
+      });
+
+    try {
+      resolve(console.log("🟢 SUCCESS : my resolve"));
+    } catch (error) {
+      reject(console.log("🟠 TRY CATCH ERROR : my reject :", error));
+    }
+  });
+};
+
+//? --------------------------------------------------
+
+//? Exécution du script gestion Co2.
+(async function gestionHumiditeHandler() {
+  try {
+    await switchEntreDeveloppementEtProduction();
+    await demandeDeMesureMaster();
+    await recuperationDeLaConsigneCo2();
+    await calculeDuDeltaCo2();
+    await constructionDeLaValeurDeLaxeX();
+    await enregistrementEnBaseDeDonnes();
+  } catch (error) {
+    console.error(
+      "🟠 Erreur dans le processus d'exécution du script gestion Co2",
+      JSON.stringify(error)
+    );
+  }
+})();
